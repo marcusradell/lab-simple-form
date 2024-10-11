a0e6cf4cd29dc0b0759f86f941b373cfb6036cdb
Marcus Rådell <marcus@radell.net>
2024-10-11 14:49:58 +0200

    add a simple fetch call

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,9 @@ http
   .createServer(function (req, res) {
     res.writeHead(200, {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*" /* @dev First, read about security */,
+      "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+      "Access-Control-Max-Age": 2592000, // 30 days
     });
     const result = JSON.stringify({ ok: true });
     res.write(result);
